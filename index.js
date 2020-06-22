51c048590aee03bf3e0096a0f35af122ad9bd650
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-06-22 15:13:28 -0500

    started work on integrating node backend and angular frontend

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.use(
     extended: true,
   })
 )
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js and Express' })
 })
@@ -16,69 +23,55 @@ app.get('/', (request, response) => {
 
 // This is the cipher request:
 
-// app.get('/cipher', function(request, response) {
-
-//   response.json("This is the cipher function")
-
-  
-// });
-
 app.get('/cipher', function(request, response) {
 
   let text = request.query.text;
   let shift = request.query.shift;
   let encryptedText;
 
-  // This is the cipher method:
+  // This is the cipher function:
 
-    function caesarCipher(text, shift) {
+function caesarCipher(lexicon, rubicon) {
       const alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-      let textArray = text.split("");
+      let shifted = parseInt(rubicon);
+      let lexiconArray = lexicon.split("");
       let cryptArray = [];
-      textArray.forEach(function(letter, index) {
-        if (shift > alpha.length) {
-          shift = (shift % alpha.length);
+      lexiconArray.forEach(function(letter, index) {
+        if (shifted > alpha.length) {
+          shifted = (shifted % alpha.length);
         }
         if (alpha.includes(letter.toLowerCase())) {
           if (letter === letter.toLowerCase()) {
-            if (alpha.indexOf(letter) + shift < 26) {
-              cryptArray.push(alpha[alpha.indexOf(letter) + shift]);
+            if (alpha.indexOf(letter) + shifted < 26) {
+              cryptArray.push(alpha[alpha.indexOf(letter.toLowerCase()) + shifted]);
             } else { 
-              cryptArray.push(alpha[alpha.length - ((alpha.length + (alpha.length - alpha.indexOf(letter.toLowerCase()) - shift)))]);
+              cryptArray.push(alpha[alpha.length - ((alpha.length + (alpha.length - alpha.indexOf(letter.toLowerCase()) - shifted)))]);
             }
           } else {
-            if (alpha.indexOf(letter.toLowerCase()) + shift < 26) {
-              cryptArray.push((alpha[alpha.indexOf(letter.toLowerCase()) + shift]).toUpperCase());
+            if (alpha.indexOf(letter.toLowerCase()) + shifted < 26) {
+              cryptArray.push((alpha[alpha.indexOf(letter.toLowerCase()) + shifted]).toUpperCase());
             } else {
-              cryptArray.push((alpha[alpha.length - ((alpha.length + (alpha.length - alpha.indexOf(letter.toLowerCase()) - shift)))].toUpperCase()));
+              cryptArray.push((alpha[alpha.length - ((alpha.length + (alpha.length - alpha.indexOf(letter.toLowerCase()) - shifted)))].toUpperCase()));
             }
           } 
         } else {
           cryptArray.push(letter);
         }
-      });
-      whateverArray = ["a", "b"]
-      return whateverArray;
-      // return [text, shift];
-      // return cryptArray;
-      // cryptString = cryptArray.join("");
-      // return `${cryptString} and pancakes`;
-      // encryptedText = "waffle"
-      // return encryptedText;
 
-      // return cryptArray.join("");
-      // return `text: ${text} and shift ${shift} and crypt array: ${cryptString}`
-      
+      });
+      return cryptArray.join("");
     }
 
-  encryptedText = caesarCipher(text, shift);
+    encryptedText = caesarCipher(text, shift);
 
-  // Here ends the cipher method.
+  // Here ends the cipher function.
 
   response.json(`This is the text: ${text} and this is the shift value: ${shift}, and this is the encrypted text: ${encryptedText}`);
   
 });
 
+
+// Here ends the cipher request.
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
